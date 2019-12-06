eb918564d43d60d80dd0afaf495474acaccc7e22
Hazard-Nico <52674988+Hazard-Nico@users.noreply.github.com>
2019-12-06 23:46:25 +0700

    Add files via upload

---
+++ application/controllers/Auth.php
@@ -0,0 +1,88 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Lost & Found';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // Validasinya sukses
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        // var_dump($user); //cek apakah valid
+        if ($user) {
+            // User nya ada
+            if ($user['email'] == $email) {
+                // Cek password
+                if (password_verify($password, $user['password'])) { } else {
+                    // Password salah
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Wrong Password</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Email is not activated</div>');
+                redirect('auth');
+            }
+        } else {
+            // User nya tidak ada
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Email is not registered</div>');
+            redirect('auth');
+        }
+    }
+
+    public function registration()
+    {
+        $this->form_validation->set_rules('nama', 'Name', 'required|trim');
+        $this->form_validation->set_rules('username', 'UserName', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password1', 'Password', 'trim|required|min_length[6]|matches[password2]', [
+            'matches' => 'password dont match! ',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'trim|required|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            // $this->load->library('form_validation');
+            $data['title'] = 'Lost & Found Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // echo "Data berhasil ditambahkan";
+            $data = [
+                'id_level' => 1,
+                'nama' => htmlspecialchars($this->input->post('nama', true)),
+                'username' => htmlspecialchars($this->input->post('username', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulations your account has been created. Please Login</div>');
+            redirect('auth');
+        }
+    }
+}
---
+++ application/views/auth/login.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+            <form class="user" method="post" action="<?= base_url('auth/index') ?>">
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Email address" value="<?= set_value('email'); ?>" >
+                        <label for="inputEmail">Email address</label>
+                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
+                        <label for="inputPassword">Password</label>
+                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="remember-me">
+                            Remember Password
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block" href="#">Login</button>
+            </form>
+            <div class="text-center">
+                <a class="d-block small mt-3" href="auth/registration">Register an Account</a>
+                <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+            </div>
+        </div>
+    </div>
+</div>
