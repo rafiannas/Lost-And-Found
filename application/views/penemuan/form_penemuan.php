9ec4734903dec50eb8e145ca8a0fc540c69a2bca
dheanrp <neadhisa@gmail.com>
2019-12-12 22:44:15 +0700

    test

---
+++ application/views/penemuan/form_penemuan.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulir Pengambilan</title>
+</head>
+<body>
+	<center>
+		<h1>Formulir Pengambilan</h1>
+	</center>
+	<form action="<?php echo base_url(). 'pengambilan/form_pengambilan'; ?>" method="post">
+		<table style="margin:20px auto;">
+			<tr>
+				<td>No Laporan</td>
+                <td><input type="text" name="laporan"></td>
+			</tr>
+			<tr>
+				<td>Nama Pengambil</td>
+				<td><input type="text" name="nama"></td>
+			</tr>
+			<tr>
+				<td>No Handphone</td>
+				<td><input type="text" name="hp"></td>
+            </tr>
+            <tr>
+				<td>Foto Pengambil</td>
+				<td><input type="text" name="foto"></td>
+            </tr>
+            <tr>
+				<td>Tanggal Pengambilan</td>
+				<td><input type="text" name="tanggal"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Tambah"></td>
+			</tr>
+		</table>
+	</form>	
+</body>
+</html>
