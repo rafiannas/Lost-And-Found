904af3725018de2550fb67b91737b51eb237723a
Galihadgn <gadiguna@gmail.com>
2019-12-13 14:57:47 +0700

    edited by Galihadgn

--- application/views/templates/sb.php
+++ application/views/templates/sb.php
@@ -12,7 +12,7 @@
   <title>Daftar Barang Hilang</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -40,7 +40,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="">
+        <a class="nav-link" href="<?= base_url('home') ?>">
           <i class="fas fa-home"></i>
           <span>Halaman Utama</span></a>
       </li>
@@ -78,21 +78,21 @@
       <!-- End of Main Content -->
 
       <!-- Bootstrap core JavaScript-->
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="<?= base_url('assets/') ?>vendor/jquery/jquery.min.js"></script>
+      <script src="<?= base_url('assets/') ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
       <!-- Core plugin JavaScript-->
-      <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+      <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
       <!-- Custom scripts for all pages-->
-      <script src="js/sb-admin-2.min.js"></script>
+      <script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
 
       <!-- Page level plugins -->
-      <script src="vendor/chart.js/Chart.min.js"></script>
+      <script src="<?= base_url('assets/') ?>vendor/chart.js/Chart.min.js"></script>
 
       <!-- Page level custom scripts -->
-      <script src="js/demo/chart-area-demo.js"></script>
-      <script src="js/demo/chart-pie-demo.js"></script>
+      <script src="<?= base_url('assets/') ?>js/demo/chart-area-demo.js"></script>
+      <script src="<?= base_url('assets/') ?>js/demo/chart-pie-demo.js"></script>
 
 </body>
 
