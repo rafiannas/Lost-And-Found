619e5b06f804b76aff0da6db9079b8c5af2083c4
dheanrp <neadhisa@gmail.com>
2019-12-16 16:01:40 +0700

    dhean edit form pengambilan

--- application/models/Pengambilan_model.php
+++ application/models/Pengambilan_model.php
@@ -4,11 +4,11 @@ class Pengambilan_model extends CI_Model{
     
     function input_data(){
 		$data = [
-			"no_laporan" => $this->input->post('no_laporan', true),
-			"nama_pengambil" => $this->input->post('nama_pengambil', true),
-			"no_hp" => $this->input->post('no_hp', true),
-			"foto_pengambil" => $this->input->post('foto_pengambil', true),
-			"tgl_pengambilan" => $this->input->post('tgl_pengambilan', true)
+			'no_laporan' => $this->input->post('no_laporan'),
+			'nama_pengambil' => $this->input->post('nama_pengambil', true),
+			'no_hp' => $this->input->post('no_hp', true),
+			'foto_pengambil' => $this->input->post('foto_pengambil', true),
+			'tgl_pengambilan' => $this->input->post('tgl_pengambilan', true)
 		];
 
 		$this->db->insert('pengambilan', $data);
