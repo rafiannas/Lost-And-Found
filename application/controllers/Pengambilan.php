e88661b7e83b290e998f0365f26b0be10a055081
dheanrp <neadhisa@gmail.com>
2019-12-17 16:58:26 +0700

    dhean edit pengambilan

--- application/controllers/Pengambilan.php
+++ application/controllers/Pengambilan.php
@@ -6,15 +6,40 @@ class Pengambilan extends CI_Controller{
 	function __construct(){
         	parent::__construct();
 			$this->load->model('pengambilan_model');
+			$this->load->library('form_validation');
 	}
 
 	public function index()
 	{
+		#template tampilan web
 		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-		$data['title'] = 'Lost & Found - Pengambilan';
+		$data['title'] = 'Lost & Found -  Formulir Pengambilan';
 		$this->load->view('templates/auth_header', $data);
 		$this->load->view('templates/sidebar', $data);
 		$this->load->view('templates/topbar', $data);
 		$this->load->view('pengambilan/index');
+
+		#hubungan dengan data
+		$data['content_id'] = 'pengambilan/index.php';
+
+        $data = array(
+                    'no_laporan' =>set_value('no_laporan') ,
+                    'nama_pengambil' =>set_value('nama_pengambil'),
+                    'no_hp' =>set_value('no_hp'),
+                    'foto_pengambil'=>set_value('foto_pengambil'),
+                    'tgl_pengambilan'=>set_value('tgl_pengambilan'));
+
+		$this->form_validation->set_rules('no_laporan', 'no_laporan', 'required');
+        $this->form_validation->set_rules('nama_pengambil', 'nama_pengambil', 'required');
+        $this->form_validation->set_rules('no_hp', 'no_hp', 'required');
+        $this->form_validation->set_rules('foto_pengambil', 'foto_pengambil', 'required');
+        $this->form_validation->set_rules('tgl_pengambilan', 'tgl_pengambilan', 'required');
+        
+        if ($this->form_validation->run() ==FALSE) {
+            $data['content_id'] = 'pengambilan/index.php';
+            } else {
+			$this->pengambilan_model->input_data();
+			redirect(home);
+        }
 	}
 }
--- application/models/Pengambilan_model.php
+++ application/models/Pengambilan_model.php
@@ -1,18 +1,4 @@
 <?php 
- 
-class Pengambilan_model extends CI_Model{
-    
-    function input_data(){
-		$data = [
-			"no_laporan" => $this->input->post('no_laporan', true),
-			"nama_pengambil" => $this->input->post('nama_pengambil', true),
-			"no_hp" => $this->input->post('no_hp', true),
-			"foto_pengambil" => $this->input->post('foto_pengambil', true),
-			"tgl_pengambilan" => $this->input->post('tgl_pengambilan', true)
-		];
-
-if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
 
 class Pengambilan_model extends CI_Model
 {
@@ -25,8 +11,16 @@ class Pengambilan_model extends CI_Model
 	}
 
 
-	function input_data($data)
+	function input_data()
 	{
+		$data = [
+			"no_laporan" => $this->input->post('no_laporan', true),
+			"nama_pengambil" => $this->input->post('nama_pengambil', true),
+			"no_hp" => $this->input->post('no_hp', true),
+			"foto_pengambil" => $this->input->post('foto_pengambil', true),
+			"tgl_pengambilan" => $this->input->post('tgl_pengambilan', true)
+		];
+		
 		$this->db->insert($this->table, $data);
 	}
 }
